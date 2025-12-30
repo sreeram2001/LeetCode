d07058fa1288146eac64ecf28f26dbff38531354
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2025-12-30 03:12:24 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int h = nums.size();
+        int l = 0;
+
+        while(l <= h)
+        {
+            int mid = (l+h)/2;
+
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if(nums[mid] < target)
+            {
+                l = mid+1;
+            }
+            else
+            {
+                h = mid-1;
+            }
+        }
+        return -1;
+    }
+};
