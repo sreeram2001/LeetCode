6bf2ee39d0c32248bdf576f2a0510cefef9231b8
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2026-04-14 13:22:10 -0700

    Time: 0 ms (100%), Space: 42 MB (31.37%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -7,17 +7,17 @@ public:
 
         for(int i=1;i<nums.size();i++)
         {
-            prefix[i] = prefix[i-1]*nums[i-1];
+            prefix[i] = nums[i-1]*prefix[i-1];
         }
 
-        for(int i=nums.size()-2;i>-1;i--)
+        for(int i=nums.size()-2;i>=0;i--)
         {
-            suffix[i] = suffix[i+1]*nums[i+1];
+            suffix[i] = nums[i+1]*suffix[i+1];
         }
 
         for(int i=0;i<nums.size();i++)
         {
-            nums[i] = suffix[i]*prefix[i];
+            nums[i] = prefix[i]*suffix[i];
         }
 
         return nums;
