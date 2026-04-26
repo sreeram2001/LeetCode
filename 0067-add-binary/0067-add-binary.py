ed66ab0c494139a4c1ad81db20314f2bbbf4deac
Sreeram S <40134790+sreeram2001@users.noreply.github.com>
2026-04-26 15:55:34 -0700

    Time: 0 ms (100%), Space: 19.3 MB (52.5%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        
+        num1 = int(a, 2)
+        num2 = int(b, 2)
+        sumi = num1 + num2
+
+        return bin(sumi)[2:]
